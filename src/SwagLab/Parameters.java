a69cad9daf25831a2d788905a4fd8dad9543dde5
muhyealdeen ayesh <131160699+muhye98@users.noreply.github.com>
2023-11-29 17:31:28 +0300

    this assiment for ltuc

---
+++ src/SwagLab/Parameters.java
@@ -0,0 +1,10 @@
+package SwagLab;
+
+public class Parameters {
+	
+	static String myUrl = "https://www.saucedemo.com/";
+	static String UserName = "standard_user";
+	static String Passwoard = "secret_sauce";
+	
+
+}
---
+++ src/SwagLab/SwagLabTest.java
@@ -0,0 +1,62 @@
+package SwagLab;
+
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class SwagLabTest extends Parameters {
+	WebDriver driver = new ChromeDriver();
+
+	@BeforeTest
+	public void MyBeforeTest() {
+
+		driver.get(myUrl);
+		WebElement UserNameElemnt = driver.findElement(By.id("user-name"));
+		WebElement PasswordElemnt = driver.findElement(By.id("password"));
+		UserNameElemnt.sendKeys(UserName);
+		PasswordElemnt.sendKeys(Passwoard);
+		driver.findElement(By.id("login-button")).click();
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void FirstTest() {
+		List<WebElement> AddToCart = driver.findElements(By.className("btn"));
+		List<WebElement> productsName = driver.findElements(By.className("inventory_item_name"));
+		List<WebElement> PriceForItems = driver.findElements(By.className("inventory_item_price"));
+
+		for (int i = 0; i < AddToCart.size(); i++) {
+			if (productsName.get(i).getText().contains("Labs Bolt") || productsName.get(i).getText().contains("Onesie")
+					|| productsName.get(i).getText().contains("Labs Fleece")) {
+				continue;
+			} else {
+				AddToCart.get(i).click();
+				PriceForItems.get(i).getText();
+				System.out.println(PriceForItems.get(i).getText());
+				String PriceEND = PriceForItems.get(i).getText().replace("$", " ");
+				double PriceAsDouble = Double.parseDouble(PriceEND);
+				double TaxValue = 0.10;
+				double finelPrice = PriceAsDouble * TaxValue + PriceAsDouble;
+				if (finelPrice % 2 == 0) {
+					System.out.println("the final price is even number and the value of this number " + finelPrice);
+				} else {
+					System.out.println("the final price is an odd number and the value of this price " + finelPrice);
+				}
+			}
+
+		}
+	}
+
+	@AfterTest
+	public void MyAfterTest() {
+
+	}
+
+}
